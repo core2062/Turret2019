1baa74346db21bf8f945f148d9c050173336a367
njbackhaus <njbackhaus@gmail.com>
2020-01-07 20:35:25 -0600

    edited logic need to fix stops

--- src/TurretSubystem.cpp
+++ src/TurretSubystem.cpp
@@ -13,36 +13,34 @@ void TurretSubsystem::robotInit() {
     ntinst.StartClientTeam(2062);
 }
 
-void TurretSubsystem::teleopInit() { 
+void TurretSubsystem::teleopInit() {
     m_startupTurretPosition = m_turret.GetSelectedSensorPosition(0);
 }
 
 void TurretSubsystem::teleop() {
     double inputMotorValue = 0;
     auto table = ntinst.GetTable("COREVision");
-    int imageWidth = 1024;
+    int imageCenterX = 1024/2;
     bool hasCenterX = false;
-    double centerX = 0;
+    double targetCenterX = 0;
     hasCenterX = table->GetBoolean("Has Center X", false);
     if (hasCenterX) {
-        centerX = table->GetNumber("Center X", -1);
-        if( std::abs(centerX-(imageWidth/2))<50){
-            //leave zero
-        } else if(centerX<(imageWidth/2)){
-            inputMotorValue = 0.1;
-        } else if(centerX>(imageWidth/2)) {
-            inputMotorValue = -0.1;
-        } else{
-
+        targetCenterX = table->GetNumber("Center X", -1);
+        double targetCenterXOffset = imageCenterX-targetCenterX;
+        double b =  (targetCenterXOffset/imageCenterX)*0.2;
+        if(-0.075 < b && b < 0.075){
+            b = 0;
         }
+        inputMotorValue = b;
     }
     bool atLeftStop = ((m_turret.GetSelectedSensorPosition(0)) > (m_startupTurretPosition + 468.0));
     bool atRightStop = ((m_turret.GetSelectedSensorPosition(0)) < (m_startupTurretPosition - 468.0));
-
-    if ((atLeftStop && inputMotorValue < 0) || atRightStop && inputMotorValue > 0) { 
-        inputMotorValue = 0;  
+    SmartDashboard::PutBoolean("Left Stop", atLeftStop);
+    SmartDashboard::PutBoolean("Right Stop", atRightStop);
+    if ((atLeftStop && inputMotorValue > 0) || atRightStop && inputMotorValue < 0) {
+        inputMotorValue = 0;
     }
-
+    SmartDashboard::PutNumber("Motor Value", inputMotorValue);
     m_turret.Set(ControlMode::PercentOutput, inputMotorValue);
     SmartDashboard::PutNumber("Turret position", m_turret.GetSelectedSensorPosition(0));
 }
