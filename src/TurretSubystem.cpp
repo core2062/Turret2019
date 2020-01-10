0336c5ee104bde3e97bf273308169073d329711d
njbackhaus <njbackhaus@gmail.com>
2020-01-09 19:33:21 -0600

    vision tracking working ok

--- src/TurretSubystem.cpp
+++ src/TurretSubystem.cpp
@@ -19,25 +19,26 @@ void TurretSubsystem::teleopInit() {
 
 void TurretSubsystem::teleop() {
     double inputMotorValue = 0;
-    auto table = ntinst.GetTable("COREVision");
+   auto table = ntinst.GetTable("COREVision");
     int imageCenterX = 1024/2;
     bool hasCenterX = false;
     double targetCenterX = 0;
     hasCenterX = table->GetBoolean("Has Center X", false);
     if (hasCenterX) {
         targetCenterX = table->GetNumber("Center X", -1);
-        double targetCenterXOffset = imageCenterX-targetCenterX;
-        double b =  (targetCenterXOffset/imageCenterX)*0.2;
-        if(-0.075 < b && b < 0.075){
-            b = 0;
-        }
-        inputMotorValue = b;
+        double targetCenterXOffset = targetCenterX-imageCenterX;
+        inputMotorValue =  (targetCenterXOffset/imageCenterX)*0.25;
     }
-    bool atLeftStop = ((m_turret.GetSelectedSensorPosition(0)) > (m_startupTurretPosition + 468.0));
-    bool atRightStop = ((m_turret.GetSelectedSensorPosition(0)) < (m_startupTurretPosition - 468.0));
+    //positive is right
+    // inputMotorValue = (-operatorJoystick->GetAxis(CORE::COREJoystick::JoystickAxis::LEFT_STICK_X))*0.20;
+    bool atLeftStop = ((m_turret.GetSelectedSensorPosition(0)) < (m_startupTurretPosition - 468.0));
+    bool atRightStop = ((m_turret.GetSelectedSensorPosition(0)) > (m_startupTurretPosition + 468.0));
     SmartDashboard::PutBoolean("Left Stop", atLeftStop);
     SmartDashboard::PutBoolean("Right Stop", atRightStop);
-    if ((atLeftStop && inputMotorValue > 0) || atRightStop && inputMotorValue < 0) {
+    if(-0.075 < inputMotorValue && inputMotorValue < 0.075){
+        inputMotorValue = 0;
+    }
+    if ((atRightStop && inputMotorValue < 0) || (atLeftStop && inputMotorValue > 0)) {
         inputMotorValue = 0;
     }
     SmartDashboard::PutNumber("Motor Value", inputMotorValue);
